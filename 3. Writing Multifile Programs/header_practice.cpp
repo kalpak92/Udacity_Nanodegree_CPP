631ab6fa4143179da425751abee899297834c99f
Kalpak <kals9seals@gmail.com>
2020-05-17 14:08:00 -0400

    Header files and multiple header files

---
+++ 3. Writing Multifile Programs/header_practice.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include "header_practice.h"
+using std::vector;
+using std::cout;
+
+/**
+ * The #include statement for the header used quotes " " around the file name, and not angle brackets <>. 
+ * We have stored the header in the same directory as the .cpp file, and the quotes tell the preprocessor to look for the file in the same directory as the current file - not in the usual set of directories where libraries are typically stored.
+ * 
+ * Finally, there is a preprocessor directive:
+ * 
+ * #ifndef HEADER_EXAMPLE_H
+ * #define HEADER_EXAMPLE_H
+ * 
+ * at the top of the header, along with an #endif at the end. 
+ * 
+ * This is called an "include guard". 
+ * Since the header will be included into another file, and #include just pastes contents into a file, the include guard prevents the same file from being pasted multiple times into another file. 
+ * This might happen if multiple files include the same header, and then are all included into the same main.cpp, for example. The ifndef checks if HEADER_EXAMPLE_H has not been defined in the file already. 
+ * If it has not been defined yet, then it is defined with #define HEADER_EXAMPLE_H, and the rest of the header is used. 
+ * If HEADER_EXAMPLE_H has already been defined, then the preprocessor does not enter the ifndef block. 
+ * 
+ * Note: There are other ways to do this. 
+ * Another common way is to use an #pragma oncepreprocessor directive,
+*/
+
+
+int IncrementAndComputeVectorSum(vector<int> v) 
+{
+    int total = 0;
+    AddOneToEach(v);
+
+    for (auto i: v) {
+        total += i;
+    }
+    return total;
+}
+
+void AddOneToEach(vector<int> &v) 
+{
+    // Note that the function passes a reference to v
+    // and the for loop below uses references to 
+    // each item in v. This means the actual
+    // ints that v holds will be incremented.
+    for (auto& i: v) {
+        i++;
+    }
+}
+
+int main() 
+{
+    vector<int> v{1, 2, 3, 4};
+    int total = IncrementAndComputeVectorSum(v);
+    cout << "The total is: " << total << "\n";
+}
---
+++ 3. Writing Multifile Programs/header_practice.h
@@ -0,0 +1,11 @@
+// This file will be saved as "header_practice.h"
+#include <vector>
+#ifndef HEADER_PRACTICE_H
+#define HEADER_PRACTICE_H
+
+using std::vector;
+
+int IncrementAndComputeVectorSum(vector<int>);
+void AddOneToEach(vector<int>&);
+
+#endif
